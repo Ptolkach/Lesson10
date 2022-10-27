8c0b99e4e6b851b2eb2fee284678f72dfe58300d
Pavel Tolkach <pb.tolkach@outlook.com>
2022-10-27 10:17:51 +0300

    Добавьте файлы проекта.

---
+++ Task1/Angle.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Angle
+    {
+        int gradus;
+        int min;
+        int sec;
+        public int Gradus
+        {
+            get
+            {
+                return gradus;
+            }
+            set 
+            {
+                gradus = value;
+            }
+        }
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+            set
+            {
+                min = value;
+            }
+        }
+        public int Sec
+        {
+            get
+            {
+                return sec;
+            }
+            set
+            {
+                sec = value;
+            }
+        }
+
+        public Angle(int gradus, int min, int sec)
+        {
+            this.gradus = gradus;
+            this.min = min;
+            this.sec = sec;
+        }
+        public double ToRadians()
+        {
+            return gradus * Math.PI /180 + min * Math.PI / (180 * 60) + sec * Math.PI / (180 * 3600);
+        }
+    }
+}
---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите угол с указанием градусов, минут, секунд (ввести 3 целочисленных значения)");
+            int gradus = Convert.ToInt32(Console.ReadLine());
+            int min = Convert.ToInt32(Console.ReadLine());
+            int sec = Convert.ToInt32(Console.ReadLine());
+
+            Angle angle = new Angle(gradus,min,sec);
+            double radians = angle.ToRadians();
+            Console.WriteLine(radians);
+            Console.ReadKey();
+        }
+    }
+}
